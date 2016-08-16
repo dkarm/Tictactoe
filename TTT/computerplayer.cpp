0e93873ba82be8f36d25206d253baba00845419e
Dipti <diptiskarmarkar@gmail.com>
2016-08-16 18:26:43 -0500

    added a computer player

---
+++ TTT/computerplayer.cpp
@@ -0,0 +1,9 @@
+//
+//  computerplayer.cpp
+//  TTT
+//
+//  Created by Dipti Karmarkar on 8/16/16.
+//  Copyright © 2016 Dipti Karmarkar. All rights reserved.
+//
+
+#include "computerplayer.hpp"
---
+++ TTT/computerplayer.hpp
@@ -0,0 +1,14 @@
+//
+//  computerplayer.hpp
+//  TTT
+//
+//  Created by Dipti Karmarkar on 8/16/16.
+//  Copyright © 2016 Dipti Karmarkar. All rights reserved.
+//
+
+#ifndef computerplayer_hpp
+#define computerplayer_hpp
+
+#include <stdio.h>
+
+#endif /* computerplayer_hpp */
